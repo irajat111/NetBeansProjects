08576b99c085f80095320bcd5e3575e467ca592c
irajat111 <irajat111@gmail.com>
2026-01-09 23:24:13 +0900

    Initial commit

---
+++ Practice/src/main/java/com/rajat/practice/Practice.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.rajat.practice;
+import java.awt.AWTException;
+//import java.awt.Robot;
+//import java.awt.Toolkit;
+//import java.awt.datatransfer.Clipboard;
+//import java.awt.datatransfer.StringSelection;
+//import java.awt.event.KeyEvent;
+//import javax.sound.sampled.Clip;
+//import java.util.Scanner;
+
+/**
+ *
+ * @author i_rajat_1
+ */
+public class Practice {
+
+    public static void main(String[] args)  throws AWTException, InterruptedException {
+        System.out.println("Hello World!");
+        
+        MainFrame obj = new MainFrame();
+        obj.setVisible(true);
+        
+        
+//        String msg;
+//        Scanner in = new Scanner(System.in);
+//        msg = in.next().trim().toLowerCase().toUpperCase();
+//        int size = in.nextInt();
+//
+//        StringSelection stringSelection = new StringSelection(msg);
+//        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+//        clipboard.setContents(stringSelection, null);
+//
+//        Thread.sleep(1000);
+//        Robot  robot = new Robot();
+//
+//        for(int i = 1; i <= 30; i++){
+//            
+//            robot.keyPress(KeyEvent.VK_CONTROL);
+//            robot.keyPress(KeyEvent.VK_V);
+//    
+//            robot.keyRelease(KeyEvent.VK_CONTROL);
+//            robot.keyRelease(KeyEvent.VK_V);
+//
+//            robot.keyPress(KeyEvent.VK_ENTER);
+//            robot.keyRelease(KeyEvent.VK_ENTER);
+//
+//            Thread.sleep(1000);
+//        }
+        
+    }
+}
---
+++ Practice/src/main/java/com/rajat/practice/lblStatus.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rajat.practice;
+
+/**
+ *
+ * @author i_rajat_1
+ */
+class lblStatus {
+
+    static void setText(String please_fill_both_fields) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+//    MainFrame obj1 = new MainFrame();
+//    obj1.setVissible(true);
+}
